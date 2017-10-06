daeb86560c666c4640bb26b13c525784e771de58
Sara_Hsu <lookatsirius@sina.com>
2017-10-06 04:54:03 -0500

    Update CheckCode.py

--- CheckCode.py
+++ CheckCode.py
@@ -1,6 +1,22 @@
 import copy
 
 class Test(object):
+    def back2origin(self, inputlist):
+        for i in range(0,len(inputlist),1):
+            if (inputlist[i] >= 0 and inputlist[i] <= 9):
+                inputlist[i] = inputlist[i]
+            elif(inputlist[i] >= 10 and inputlist[i] <= 17):
+                inputlist[i] = chr(inputlist[i] + 55)
+            elif (inputlist[i] >= 18 and inputlist[i] <= 22):
+                inputlist[i] = chr(inputlist[i] + 56)
+            elif (inputlist[i] >= 23 and inputlist[i] <= 25):
+                inputlist[i] = chr(inputlist[i] + 57)
+            elif (inputlist[i] >= 26 and inputlist[i] <= 27):
+                inputlist[i] = chr(inputlist[i] + 58)
+            elif (inputlist[i] >= 28 and inputlist[i] <= 30):
+                inputlist[i] = chr(inputlist[i] + 59)
+        return inputlist
+
     # inputString is 15 digit, mode 1 is nonZero, mode 2 sets digit 7&8 Zero
     def CheckCode(self, inputString, mode):
         # throw input error if inputString is not 15 digits
@@ -23,6 +39,7 @@ class Test(object):
             elif (ord(merged[i]) >= 87 and ord(merged[i]) <= 89 ):
                 merged[i] = ord(merged[i]) - 59
             # elif other letter throw input error
+        # print("merged", merged)
         wi = [1,3,9,27,19,26,16,17,20,29,25,13,8,24,10,30,28]
 
         if mode == 1:
@@ -30,49 +47,37 @@ class Test(object):
             cixwi = list(map(lambda x: x[0] * x[1], zip(wi, merged)))
             sumup = sum(cixwi[0:len(cixwi)])
             rest = 31 - sumup % 31
-            if (rest >= 0 and rest <= 9):
-                rest = rest
-            elif(rest >= 10 and rest <= 17):
-                rest = chr(rest+ 55)
-            elif (rest >= 18 and rest <= 22):
-                rest = chr(rest + 56)
-            elif (rest >= 23 and rest <= 25):
-                rest = chr(rest + 57)
-            elif (rest >= 26 and rest <= 27):
-                rest = chr(rest + 58)
-            elif (rest >= 28 and rest <= 30):
-                rest = chr(rest + 59)
             merged1 = copy.deepcopy(merged)
             merged1.append(rest)
+            # print("merged1", merged1)
+            self.back2origin(merged1)
             newmerged1 = ''.join(str(e) for e in merged1)
-
-            # print(newmerged1)
+            # print("newmerged1", newmerged1)
             return newmerged1
 
         elif mode == 2:
-            # CheckCode2: make 7&8 digit 0&0
             merged2 = copy.deepcopy(merged)
-            merged2[6:8] = [0,0]
+            # if ZhiXiaShi
+            # CheckCode2: make 678 digit 000
+            if merged2[2:5] == '110' or '120' or '310' or '500':
+                merged2[5:8] = [0,0,0]
+
+            # if not ZhiXiaShi
+            # CheckCode2: make 78 digit 00
+            else:
+                merged2[6:8] = [0,0]
+
             cixwi2 = list(map(lambda x: x[0] * x[1], zip(wi, merged2)))
             sumup2 = sum(cixwi2[0:len(cixwi2)])
             rest2 = 31 - sumup2 % 31
-            if (rest2 >= 0 and rest2 <= 9):
-                rest2 = rest2
-            elif(rest2 >= 10 and rest2 <= 17):
-                rest2 = chr(rest2 + 55)
-            elif (rest2 >= 18 and rest2 <= 22):
-                rest2 = chr(rest2 + 56)
-            elif (rest2 >= 23 and rest2 <= 25):
-                rest2 = chr(rest2 + 57)
-            elif (rest2 >= 26 and rest2 <= 27):
-                rest2 = chr(rest2 + 58)
-            elif (rest2 >= 28 and rest2 <= 30):
-                rest2 = chr(rest2 + 59)
             merged2.append(rest2)
+            # print("merged2", merged2)
+            self.back2origin(merged2)
             newmerged2 = ''.join(str(e) for e in merged2)
-
-            # print(newmerged2)
+            # print("newmerged2", newmerged2)
             return newmerged2
 
 # if __name__ == '__main__':
-#     print (Test().CheckCode("22012266874988X",1))
+#     print (Test().CheckCode("22012266874988X", 1))
+#     print (Test().CheckCode("L11010210110532", 1))
+#     print (Test().CheckCode("110100625906144",2))
