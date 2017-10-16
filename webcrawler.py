c153f80278ac5f0666c535911e7649449c267a56
Sara_Hsu <lookatsirius@sina.com>
2017-10-16 01:00:51 -0500

    Update webcrawler.py

--- webcrawler.py
+++ webcrawler.py
@@ -5,20 +5,21 @@ import pymysql
 from sshtunnel import SSHTunnelForwarder
 
 # url：只需替换keyword（1 如果为Null则跳过 2 如果第一个成功则跳过第二个）
-def get_url(self, keyword):
+def get_url(keyword):
     url_pre = 'http://www.creditchina.gov.cn/api/credit_info_search?keyword='
     url_pro = '&templateId=&page=1&pageSize=10'
+    keyword = str(keyword)
     # data = str(int(time.time() * 1000))
     url = url_pre + keyword + url_pro
     return url
 
 # 如果提取到网页上特定字段："'results' = []" 则为不成功
-def get_webkey(self, url):
+def get_webkey(url):
+
     data = requests.get(url)
     data1 = data.json()
     a = data1.get('data')
     b = a.get('results')
-    # print("check = ", check)
     if b != []:
         b = url[61:79]
         return b
@@ -33,25 +34,31 @@ select * from TaxationCode
 # 建表时 charset utf8mb4
 sql_create = """
 CREATE TABLE CheckedTaxationCode (
-zipCode CHAR(6),
 companyName CHAR(50),
 creditCode CHAR(18),
+zipCode CHAR(6),
 gotCompanyInfo CHAR(2))
 DEFAULT CHARSET=utf8mb4
 """
 
 sql_insert = """
-INSERT INTO CheckedTaxationCode(gotCompanyInfo,
-       zipCode, companyName, creditCode)
+INSERT INTO CheckedTaxationCode(companyName, creditCode, zipCode, gotCompanyInfo)
        VALUES ('%s', '%s', '%s', '%s' )"""
 
 if __name__ == '__main__':
     server = SSHTunnelForwarder(
-
+        'xxx.xx.xxx.xx',
+        ssh_username='xxx',
+        ssh_password='xxx',
+        remote_bind_address=('127.0.0.1', 3306)
     )
     server.start()
     conn = pymysql.connect(
-
+        host='127.0.0.1',
+        port=server.local_bind_port,
+        user='root',
+        password='xxx',
+        db='xxx'
     )
     cur = conn.cursor()
 
@@ -63,55 +70,54 @@ if __name__ == '__main__':
     # 需不需要提交到数据库执行？
     # conn.commit()
 
-    try:
-        cur.execute(sql_get)
-        data = cur.fetchall()
-        print(len(data))
-        for i in range(2):#len(data):
-            print(i)
-            taxPersonCode = data[i][0]
-            companyName = data[i][1]
-            unifiedCreditCodeZero = data[i][2]
-            unifiedCreditCodeNotZero = data[0][3]
-            print((data[i][0]))
+    # try:
+    cur.execute(sql_get)
+    data = cur.fetchall()
+    for i in range(len(data)):
+        taxPersonCode = data[i][0]
+        companyName = data[i][1]
+        unifiedCreditCodeZero = data[i][2]
+        unifiedCreditCodeNotZero = data[0][3]
 
-            # 如果第一个成功则跳过第二个，直接返回第一个；如果第一个不成功则换第二个keyword，第二个成功则返回第二个；如果第二个也不成功则返回Null
-            # 保存统一社会信用编码&公司名称&地区编码（第3-8位）
-            num1 = unifiedCreditCodeZero
-            num2 = unifiedCreditCodeNotZero
-            newtable = ['-1']
-            newtable.append(companyName)
-            if num1 != 'NULL':
-                url1 = get_url(num1)
-                url2 = get_url(num2)
-                if get_webkey(url1) != 0:
-                    checkcode = num1
-                    zipCode = num1[2:8]
-                elif get_webkey(url1) == 0:
-                    if get_webkey(url2) != 0:
-                        checkcode = num2
-                        zipCode = num2[2:8]
-                    elif get_webkey(url2) == 0:
-                        checkcode = 'NULL'
-                        zipCode = 'NULL'
-            else:
-                checkcode = 'NULL'
-                zipCode = 'NULL'
-            newtable.append(checkcode)
-            newtable.append(zipCode)
-            try:
-                # 执行sql语句
-                cur.execute(sql_insert%(newtable[0], newtable[1], newtable[2], newtable[3]))
-                # 执行数据库操作每步都要数据库db.commit()以后才能操作
-                # 提交到数据库执行
-                conn.commit()
-                print(i)
-            except:
-                # Rollback in case there is any error
-                conn.rollback()
-                print('Fail to insert row ', i)
-    except:
-        print('Fail to reach the next row!')
+        # 如果第一个成功则跳过第二个，直接返回第一个；如果第一个不成功则换第二个keyword，第二个成功则返回第二个；如果第二个也不成功则返回Null
+        # 保存统一社会信用编码&公司名称&地区编码（第3-8位）
+        # num1 = '\'%s\''%unifiedCreditCodeZero
+        num1 = str(unifiedCreditCodeZero)
+        num2 = unifiedCreditCodeNotZero
+        newtable = ['-1']
+        name = companyName
+        newtable.append(name)
+        if num1 != 'NULL':
+            url1 = get_url(num1)
+            url2 = get_url(num2)
+            if get_webkey(url1) != 0:
+                checkcode = num1
+                zipCode = num1[2:8]
+            elif get_webkey(url1) == 0:
+                if get_webkey(url2) != 0:
+                    checkcode = num2
+                    zipCode = num2[2:8]
+                elif get_webkey(url2) == 0:
+                    checkcode = 'NULL'
+                    zipCode = 'NULL'
+        else:
+            checkcode = 'NULL'
+            zipCode = 'NULL'
+        newtable.append(checkcode)
+        newtable.append(zipCode)
+        try:
+            # 执行sql语句
+            cur.execute(sql_insert%(newtable[1], newtable[2], newtable[3], newtable[0]))
+            # 执行数据库操作每步都要数据库db.commit()以后才能操作
+            # 提交到数据库执行
+            conn.commit()
+            print(i)
+        except:
+            # Rollback in case there is any error
+            conn.rollback()
+            print('Fail to insert row ', i)
+    # except:
+    #     print('Fail to reach the next row!')
 
     cur.close()
     # 关闭数据库连接
