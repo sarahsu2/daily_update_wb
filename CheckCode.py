efd5eef2ef9710184d914896f59247a9509f5c17
Sara_Hsu <lookatsirius@sina.com>
2017-10-31 04:26:02 -0500

    Update CheckCode.py

--- CheckCode.py
+++ CheckCode.py
@@ -1,3 +1,6 @@
+# mode1 原始数据 mode2 78位置零 mode3 5678位置零
+# TODO: 穷举5678位, 前缀换成92, 前缀换成93
+
 import copy
 
 class Test(object):
@@ -21,7 +24,7 @@ class Test(object):
     def CheckCode(self, inputString, mode):
         # throw input error if inputString is not 15 digits
         # throw input error if inputString includes non number or capital letter digits
-
+        # print("inputString ",inputString)
         inputList = list(inputString)
         head = ['9','1']
         merged = head + inputList
@@ -38,45 +41,60 @@ class Test(object):
                 merged[i] = ord(merged[i]) - 58
             elif (ord(merged[i]) >= 87 and ord(merged[i]) <= 89 ):
                 merged[i] = ord(merged[i]) - 59
+
             # elif other letter throw input error
         # print("merged", merged)
         wi = [1,3,9,27,19,26,16,17,20,29,25,13,8,24,10,30,28]
 
+        error = 'NULL'
+
         if mode == 1:
-            # CheckCode1: use origin code
-            cixwi = list(map(lambda x: x[0] * x[1], zip(wi, merged)))
-            sumup = sum(cixwi[0:len(cixwi)])
-            rest = 31 - sumup % 31
-            merged1 = copy.deepcopy(merged)
-            merged1.append(rest)
-            # print("merged1", merged1)
-            self.back2origin(merged1)
-            newmerged1 = ''.join(str(e) for e in merged1)
-            # print("newmerged1", newmerged1)
-            return newmerged1
+            try:
+                # CheckCode1: use origin code
+                cixwi = list(map(lambda x: x[0] * x[1], zip(wi, merged)))
+                sumup = sum(cixwi[0:len(cixwi)])
+                rest = 31 - sumup % 31
+                merged1 = copy.deepcopy(merged)
+                merged1.append(rest)
+                # print("merged1", merged1)
+                self.back2origin(merged1)
+                newmerged1 = ''.join(str(e) for e in merged1)
+                # print("newmerged1", newmerged1)
+                return newmerged1
+            except:
+                return error
 
         elif mode == 2:
-            merged2 = copy.deepcopy(merged)
-            # if ZhiXiaShi
-            # CheckCode2: make 678 digit 000
-            if merged2[2:5] == [1,1,0] or merged2[2:5] == [1,2,0] or merged2[2:5] == [3,1,0] or merged2[2:5] == [5,0,0]:
-                merged2[5:8] = [0,0,0]
+            try:
+                merged2 = copy.deepcopy(merged)
+                # print("mode2", merged2)
+                # if ZhiXiaShi
+                # CheckCode2: make 678 digit 000
+                # print(merged2[2:5])
+                # print(merged2[2:5] == [1,1,0] or merged2[2:5] ==[1,2,0] or merged2[2:5] ==[3,1,0] or merged2[2:5] ==[5,0,0])
+                if merged2[2:5] == [1,1,0] or merged2[2:5] == [1,2,0] or merged2[2:5] == [3,1,0] or merged2[2:5] == [5,0,0]:
+                    merged2[5:8] = [0,0,0]
+                    # print("000")
 
-            # if not ZhiXiaShi
-            # CheckCode2: make 78 digit 00
-            else:
-                merged2[6:8] = [0,0]
+                # if not ZhiXiaShi
+                # CheckCode2: make 78 digit 00
+                else:
+                    merged2[6:8] = [0,0]
+                    # print("merged2 ",merged2)
+                    # print("00")
 
-            cixwi2 = list(map(lambda x: x[0] * x[1], zip(wi, merged2)))
-            sumup2 = sum(cixwi2[0:len(cixwi2)])
-            rest2 = 31 - sumup2 % 31
-            merged2.append(rest2)
-            # print("merged2", merged2)
-            self.back2origin(merged2)
-            newmerged2 = ''.join(str(e) for e in merged2)
-            # print("newmerged2", newmerged2)
-            return newmerged2
-                
+                cixwi2 = list(map(lambda x: x[0] * x[1], zip(wi, merged2)))
+                sumup2 = sum(cixwi2[0:len(cixwi2)])
+                rest2 = 31 - sumup2 % 31
+                merged2.append(rest2)
+                # print("merged2", merged2)
+                self.back2origin(merged2)
+                newmerged2 = ''.join(str(e) for e in merged2)
+                # print("newmerged2", newmerged2)
+                return newmerged2
+            except:
+                return error
+
         elif mode == 3:
             try:
                 merged3 = copy.deepcopy(merged)
@@ -95,3 +113,4 @@ class Test(object):
 #     print (Test().CheckCode("22012266874988X", 1))
 #     print (Test().CheckCode("L11010210110532", 1))
 #     print (Test().CheckCode("110100625906144",2))
+#     print (Test().CheckCode("330227681055129", 3))
